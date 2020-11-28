8b2f0ecbaed33bb39f39454ad29dfeadafb6eda1
jusjinuk <58197958+jusjinuk@users.noreply.github.com>
2020-11-29 00:33:44 +0900

    Added Application File; Allowed internet-permission in Manifest File.

---
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/PodoMarketApplication.kt
@@ -0,0 +1,9 @@
+package com.android.example.podomarket
+
+import android.app.Application
+
+class PodoMarketApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
