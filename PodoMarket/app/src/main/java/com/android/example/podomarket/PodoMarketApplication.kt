dcc645fd3d1d532e55f48a06523d55fa89ebd959
jusjinuk <58197958+jusjinuk@users.noreply.github.com>
2020-11-29 21:12:24 +0900

    Added basic settings for koin di.

--- PodoMarket/app/src/main/java/com/android/example/podomarket/PodoMarketApplication.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/PodoMarketApplication.kt
@@ -1,9 +1,14 @@
 package com.android.example.podomarket
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class PodoMarketApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        startKoin {
+            androidContext(this@PodoMarketApplication)
+        }
     }
 }
