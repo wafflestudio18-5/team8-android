7e00f93b055266734dbaf6dd27be30a1789ad533
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-05 18:14:16 +0900

    Add DataBinding to ProductDetailActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductDetailActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductDetailActivity.kt
@@ -2,11 +2,23 @@ package com.android.example.podomarket.ui.product
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.ActivityMainBinding
+import com.android.example.podomarket.databinding.ActivityProductDetailBinding
 
 class ProductDetailActivity : AppCompatActivity() {
+    private val binding: ActivityProductDetailBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_product_detail
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_detail)
+        binding.run {
+            
+        }
     }
 }
