1ba5a32b224d011b90b1568476cb93b92d8a73b2
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-05 18:10:46 +0900

    Write activity_product_detail.xml

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductCreateActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductCreateActivity.kt
@@ -18,7 +18,7 @@ class ProductCreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.run {
-            
+
         }
     }
 }
