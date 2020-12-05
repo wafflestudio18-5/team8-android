79737b0415e3aebbc23b11ee6b2ff92d305feba5
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-05 18:34:23 +0900

    Add import line to ProfileActivity
    Add import line for DataBinding

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/ProfileActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/ProfileActivity.kt
@@ -3,6 +3,8 @@ package com.android.example.podomarket.ui.user
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.example.podomarket.R
+import androidx.databinding.DataBindingUtil
+import com.android.example.podomarket.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
     private val binding: ActivityProfileBinding by lazy {
