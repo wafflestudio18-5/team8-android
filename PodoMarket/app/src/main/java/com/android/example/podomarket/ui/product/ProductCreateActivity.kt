aee3c044cf7967222456108ff50ee9da6ae0d5a0
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-05 17:42:17 +0900

    Add DataBinding to ProductCreateActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductCreateActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductCreateActivity.kt
@@ -2,11 +2,23 @@ package com.android.example.podomarket.ui.product
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.ActivityMainBinding
+import com.android.example.podomarket.databinding.ActivityProductCreateBinding
 
 class ProductCreateActivity : AppCompatActivity() {
+    private val binding: ActivityProductCreateBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_product_create
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_create)
+        binding.run {
+            
+        }
     }
 }
