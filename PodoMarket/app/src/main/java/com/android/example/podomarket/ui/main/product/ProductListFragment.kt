5951f534fe4728a698efb905003605297a54a7b7
jusjinuk <58197958+jusjinuk@users.noreply.github.com>
2020-12-05 17:32:56 +0900

    Add navigation, backstack logic to ProductFragment.

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/main/product/ProductListFragment.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/main/product/ProductListFragment.kt
@@ -2,9 +2,11 @@ package com.android.example.podomarket.ui.main.product
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.android.example.podomarket.R
 import com.android.example.podomarket.databinding.FragmentProductListBinding
 
@@ -28,5 +30,15 @@ class ProductListFragment : Fragment() {
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.app_bar_fragment_product_list, menu)
         super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.search_button -> Toast.makeText(activity, "Search", Toast.LENGTH_SHORT).show()
+            R.id.category_button -> findNavController().navigate(R.id.action_productListFragment_to_productCategoryFragment)
+            R.id.alert_button -> Toast.makeText(activity, "Alert", Toast.LENGTH_SHORT).show()
+            else -> throw Error("Not valid menu item for in toolbar.")
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
