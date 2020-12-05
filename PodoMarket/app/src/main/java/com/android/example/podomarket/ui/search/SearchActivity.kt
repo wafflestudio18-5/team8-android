4caa207294a2d6c8198ea42e4f6c5e3e6d7019c3
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-05 16:24:16 +0900

    Add DataBinding to SearchActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/SearchActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/SearchActivity.kt
@@ -2,11 +2,23 @@ package com.android.example.podomarket.ui.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.ActivityMainBinding
+import com.android.example.podomarket.databinding.ActivitySearchBinding
 
 class SearchActivity : AppCompatActivity() {
+    private val binding: ActivitySearchBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_search
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search)
+        binding.run {
+            
+        }
     }
 }
