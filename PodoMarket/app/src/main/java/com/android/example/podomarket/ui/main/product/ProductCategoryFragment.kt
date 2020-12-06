6a05cd5fa9c96d76990b4fe1d11d93a39836ee4d
jusjinuk <58197958+jusjinuk@users.noreply.github.com>
2020-12-06 17:40:58 +0900

    Change place where navigateUp is managed.

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/main/product/ProductCategoryFragment.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/main/product/ProductCategoryFragment.kt
@@ -2,11 +2,13 @@ package com.android.example.podomarket.ui.main.product
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.android.example.podomarket.R
 import com.android.example.podomarket.databinding.FragmentProductCategoryBinding
 
@@ -24,8 +26,16 @@ class ProductCategoryFragment : Fragment() {
             (activity as AppCompatActivity).setSupportActionBar(toolBar)
             (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
             (activity as AppCompatActivity).supportActionBar?.setDisplayShowHomeEnabled(true)
+            setHasOptionsMenu(true)
         }
         return binding.root
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> findNavController().navigateUp()
+            else -> throw Error("Not valid menu item for in toolbar.")
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
