ba71dfc8642d80b34a65c6230342acdaccdd9655
jusjinuk <58197958+jusjinuk@users.noreply.github.com>
2020-12-18 18:43:54 +0900

    Change omitted activity-owned app bar in 4a00bd2a to fragment-owned app bar.

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/main/product/ProductListFragment.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/main/product/ProductListFragment.kt
@@ -3,7 +3,6 @@ package com.android.example.podomarket.ui.main.product
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -22,43 +21,20 @@ class ProductListFragment : Fragment() {
             DataBindingUtil.inflate(inflater, R.layout.fragment_product_list, container, false)
 
         binding.run {
-            (activity as AppCompatActivity).setSupportActionBar(toolBar)
-            setHasOptionsMenu(true)
+            toolBar.apply {
+                inflateMenu(R.menu.app_bar_fragment_product_list)
+                setOnMenuItemClickListener {
+                    when (it.itemId) {
+                        R.id.search_button -> Toast.makeText(activity, "Search", Toast.LENGTH_SHORT)
+                            .show()
+                        R.id.category_button -> findNavController().navigate(R.id.action_productListFragment_to_productCategoryFragment)
+                        R.id.alert_button -> findNavController().navigate(R.id.action_productListFragment_to_inboxFragment)
+                        else -> return@setOnMenuItemClickListener false
+                    }
+                    return@setOnMenuItemClickListener true
+                }
+            }
         }
         return binding.root
-    }
-
-    override fun onResume() {
-        controlAddProductFab(true)
-        super.onResume()
-    }
-
-    override fun onPause() {
-        controlAddProductFab(false)
-        super.onPause()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.app_bar_fragment_product_list, menu)
-        super.onCreateOptionsMenu(menu, inflater)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.search_button -> Toast.makeText(activity, "Search", Toast.LENGTH_SHORT).show()
-            R.id.category_button -> findNavController().navigate(R.id.action_productListFragment_to_productCategoryFragment)
-            R.id.alert_button -> findNavController().navigate(R.id.action_productListFragment_to_inboxFragment)
-            else -> throw Error("Not valid menu item for in toolbar.")
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
-    private fun controlAddProductFab(toggle: Boolean) {
-        binding.run {
-            if (toggle)
-                addProductFab.show()
-            else
-                addProductFab.hide()
-        }
     }
 }
