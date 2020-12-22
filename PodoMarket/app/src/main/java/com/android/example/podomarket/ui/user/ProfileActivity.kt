9ff0a54b5271324710f1c4f3e57c5e46661b58b0
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-22 22:01:22 +0900

    Update ProfileActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/ProfileActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/ProfileActivity.kt
@@ -1,14 +1,19 @@
 package com.android.example.podomarket.ui.user
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.android.example.podomarket.R
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.findNavController
 import com.android.example.podomarket.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+    companion object {
+        fun intent(context: Context): Intent = Intent(context, ProfileActivity::class.java)
+    }
+
     private val binding: ActivityProfileBinding by lazy {
         DataBindingUtil.setContentView(
             this,
@@ -23,6 +28,7 @@ class ProfileActivity : AppCompatActivity() {
                 tb.inflateMenu(R.menu.app_bar_activity_profile)
                 tb.setOnMenuItemClickListener {
                     when(it.itemId) {
+                        R.id.navigate_up_button -> finish()
                         R.id.share_button -> Toast.makeText(applicationContext, "공유하기 기능 미완성", Toast.LENGTH_SHORT)
                         else -> return@setOnMenuItemClickListener false
                     }
