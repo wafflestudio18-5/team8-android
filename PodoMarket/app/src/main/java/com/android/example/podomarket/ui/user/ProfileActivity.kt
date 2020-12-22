e0a87d01490a8eafe6909b52c21d610971b70c99
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-22 20:23:08 +0900

    Add AppBar to ProfileActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/ProfileActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/ProfileActivity.kt
@@ -2,8 +2,10 @@ package com.android.example.podomarket.ui.user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.android.example.podomarket.R
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.android.example.podomarket.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
@@ -17,7 +19,17 @@ class ProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.run {
-
+            toolBar.also { tb ->
+                tb.inflateMenu(R.menu.app_bar_activity_profile)
+                tb.setOnMenuItemClickListener {
+                    when(it.itemId) {
+                        R.id.share_button -> Toast.makeText(applicationContext, "공유하기 기능 미완성", Toast.LENGTH_SHORT)
+                        else -> return@setOnMenuItemClickListener false
+                    }
+                    return@setOnMenuItemClickListener true
+                }
+            }
         }
     }
+
 }
