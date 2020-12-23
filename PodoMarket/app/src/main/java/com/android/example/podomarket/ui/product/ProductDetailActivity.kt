81adf072e9d15a6402c93b154a33a1c07290045b
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-24 00:13:25 +0900

    Add AppBar to ProductDetailActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductDetailActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductDetailActivity.kt
@@ -2,6 +2,7 @@ package com.android.example.podomarket.ui.product
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.R
 import com.android.example.podomarket.databinding.ActivityMainBinding
@@ -18,7 +19,19 @@ class ProductDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.run {
-
+            toolBar.also { tb ->
+                tb.inflateMenu(R.menu.app_bar_activity_product_detail)
+                tb.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+                tb.setNavigationOnClickListener { finish() }
+                tb.setOnMenuItemClickListener {
+                    when(it.itemId) {
+                        R.id.share_button -> Toast.makeText(applicationContext, "공 기능 미완성", Toast.LENGTH_SHORT)
+                        R.id.more_button -> Toast.makeText(applicationContext, "추가 기능 미완성", Toast.LENGTH_SHORT)
+                        else -> return@setOnMenuItemClickListener false유
+                    }
+                    return@setOnMenuItemClickListener true
+                }
+            }
         }
     }
 }
