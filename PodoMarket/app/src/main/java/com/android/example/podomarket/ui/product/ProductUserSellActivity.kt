9630e4e7d70364b8f67234bcb4ec43e57d54b6bf
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-24 01:09:33 +0900

    Add DataBinding to ProductUserSellActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductUserSellActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductUserSellActivity.kt
@@ -2,11 +2,22 @@ package com.android.example.podomarket.ui.product
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.ActivityProductUserSellBinding
 
 class ProductUserSellActivity : AppCompatActivity() {
+    private val binding: ActivityProductUserSellBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_product_user_sell
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_user_sell)
+        binding.run {
+
+        }
     }
 }
