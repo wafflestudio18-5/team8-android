ab6e2a351b1d0dd13b988f25cd09bcb079bf688e
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-24 00:15:52 +0900

    Add Incoming Intent Function to ProductDetailActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductDetailActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductDetailActivity.kt
@@ -1,5 +1,7 @@
 package com.android.example.podomarket.ui.product
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
@@ -9,6 +11,10 @@ import com.android.example.podomarket.databinding.ActivityMainBinding
 import com.android.example.podomarket.databinding.ActivityProductDetailBinding
 
 class ProductDetailActivity : AppCompatActivity() {
+    companion object {
+        fun intent(context: Context): Intent = Intent(context, ProductCreateActivity::class.java)
+    }
+
     private val binding: ActivityProductDetailBinding by lazy {
         DataBindingUtil.setContentView(
             this,
