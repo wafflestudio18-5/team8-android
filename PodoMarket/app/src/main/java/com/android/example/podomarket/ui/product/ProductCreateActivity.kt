a14acd2066655032f1e81d708c4324af87e5b39a
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-23 23:38:56 +0900

    Add AppBar to ProductCreateActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductCreateActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductCreateActivity.kt
@@ -2,6 +2,7 @@ package com.android.example.podomarket.ui.product
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.R
 import com.android.example.podomarket.databinding.ActivityMainBinding
@@ -18,7 +19,17 @@ class ProductCreateActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.run {
-
+            toolBar.also { tb ->
+                tb.inflateMenu(R.menu.app_bar_activity_product_create)
+                tb.setOnMenuItemClickListener {
+                    when(it.itemId) {
+                        R.id.close_button -> finish()
+                        R.id.complete_button -> Toast.makeText(applicationContext, "상품 등록 완료(미완성)", Toast.LENGTH_SHORT)
+                        else -> return@setOnMenuItemClickListener false
+                    }
+                    return@setOnMenuItemClickListener true
+                }
+            }
         }
     }
 }
