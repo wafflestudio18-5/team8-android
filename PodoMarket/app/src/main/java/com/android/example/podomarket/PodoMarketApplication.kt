7c58d9bb99a2820551afa0c48faad055f52eade8
jusjinuk <58197958+jusjinuk@users.noreply.github.com>
2020-12-24 23:45:46 +0900

    Add KakaoSdk to the app.

--- PodoMarket/app/src/main/java/com/android/example/podomarket/PodoMarketApplication.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/PodoMarketApplication.kt
@@ -1,6 +1,9 @@
 package com.android.example.podomarket
 
 import android.app.Application
+import com.kakao.auth.IApplicationConfig
+import com.kakao.auth.KakaoAdapter
+import com.kakao.auth.KakaoSDK
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -10,5 +13,12 @@ class PodoMarketApplication : Application() {
         startKoin {
             androidContext(this@PodoMarketApplication)
         }
+        KakaoSDK.init(
+            object : KakaoAdapter() {
+                override fun getApplicationConfig(): IApplicationConfig {
+                    return IApplicationConfig { this@PodoMarketApplication }
+                }
+            }
+        )
     }
 }
