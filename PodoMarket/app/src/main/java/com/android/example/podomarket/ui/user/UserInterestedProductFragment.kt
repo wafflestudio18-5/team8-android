ba05eebb1f09f854a5994a34b15dfe66ba91c718
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-25 11:29:14 +0900

    Add DataBinding to Fragment for UserInterestedActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/UserInterestedProductFragment.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/UserInterestedProductFragment.kt
@@ -5,15 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.FragmentUserInterestedProductBinding
 
 class UserInterestedProductFragment : Fragment() {
 
+    lateinit var binding: FragmentUserInterestedProductBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_user_interested_product, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_user_interested_product, container, false)
+
+        return binding.root
     }
 
 }
