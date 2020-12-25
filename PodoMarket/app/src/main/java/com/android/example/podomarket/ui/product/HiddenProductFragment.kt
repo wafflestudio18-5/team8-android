84833568bd72aaa913dba14debce707d1272769f
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-25 11:04:04 +0900

    Add DataBinding to Fragments for ProductUserSellActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/HiddenProductFragment.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/HiddenProductFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.FragmentHiddenProductBinding
 
 class HiddenProductFragment : Fragment() {
 
+    lateinit var binding: FragmentHiddenProductBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_hidden_product, container, false)
+
+        return binding.root
     }
 
 }
--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/SellingProductFragment.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/SellingProductFragment.kt
@@ -4,15 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.FragmentSellingProductBinding
 
 class SellingProductFragment : Fragment() {
 
+    lateinit var binding: FragmentSellingProductBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_selling_product, container, false)
+
+        return binding.root
     }
 
 }
