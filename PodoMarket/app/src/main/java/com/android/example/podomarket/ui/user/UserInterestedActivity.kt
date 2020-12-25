c0f84576a9ba0dcd23f07b22479209f03670729b
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-25 11:10:31 +0900

    Add DataBinding to UserInterestedActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/UserInterestedActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/UserInterestedActivity.kt
@@ -2,11 +2,22 @@ package com.android.example.podomarket.ui.user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.ActivityUserInterestedBinding
 
 class UserInterestedActivity : AppCompatActivity() {
+    private val binding: ActivityUserInterestedBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_user_interested
+        )
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_interested)
+        binding.run {
+
+        }
     }
 }
