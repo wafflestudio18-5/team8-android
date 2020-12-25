a89c6186156b040310fe65e22bfff48cc9244eb6
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-25 20:00:48 +0900

    Add incoming intent function to ChatRoomActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/chat/ChatRoomActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/chat/ChatRoomActivity.kt
@@ -1,5 +1,7 @@
 package com.android.example.podomarket.ui.chat
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.android.example.podomarket.R
@@ -7,6 +9,10 @@ import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.databinding.ActivityChatRoomBinding
 
 class ChatRoomActivity : AppCompatActivity() {
+    companion object {
+        fun intentWithChatRoomId(chatRoomId: Long, context: Context): Intent = Intent(context, ChatRoomActivity::class.java)
+    }
+
     private val binding: ActivityChatRoomBinding by lazy {
         DataBindingUtil.setContentView(
             this,
