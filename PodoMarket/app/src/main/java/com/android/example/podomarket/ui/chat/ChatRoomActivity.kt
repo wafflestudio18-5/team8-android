911df3409f2e716367409f4a57579db720889843
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-25 23:16:37 +0900

    Add AppBar tro ChatRoomActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/chat/ChatRoomActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/chat/ChatRoomActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.android.example.podomarket.R
 import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.databinding.ActivityChatRoomBinding
@@ -23,7 +24,18 @@ class ChatRoomActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.run {
-
+            toolBar.also { tb ->
+                tb.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+                tb.setNavigationOnClickListener { finish() }
+                tb.inflateMenu(R.menu.app_bar_activity_chat_room)
+                tb.setOnMenuItemClickListener {
+                    when(it.itemId) {
+                        R.id.more_button -> Toast.makeText(applicationContext, "추가 기능 미완성", Toast.LENGTH_SHORT)
+                        else -> return@setOnMenuItemClickListener false
+                    }
+                    return@setOnMenuItemClickListener true
+                }
+            }
         }
     }
 }
