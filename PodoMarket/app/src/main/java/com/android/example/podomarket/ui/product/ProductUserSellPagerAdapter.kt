3a28075c3a250ae5570ae25acf68bafef818ca7f
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-26 12:54:54 +0900

    Modify ProductUserSellPagerAdapter

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductUserSellPagerAdapter.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductUserSellPagerAdapter.kt
@@ -3,18 +3,16 @@ package com.android.example.podomarket.ui.product
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.android.example.podomarket.ui.product.ProductUserSellPageConst.HIDDEN_PRODUCT
-import com.android.example.podomarket.ui.product.ProductUserSellPageConst.SELLING_PRODUCT
-import com.android.example.podomarket.ui.product.ProductUserSellPageConst.SOLD_PRODUCT
+import com.android.example.podomarket.ui.product.ProductUserSellPageConst.MY_SALES
+import com.android.example.podomarket.ui.product.ProductUserSellPageConst.OTHER_SALES
 
 class ProductUserSellPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
-    override fun getItemCount(): Int = 3
+    override fun getItemCount(): Int = 2
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            SELLING_PRODUCT -> SellingProductFragment()
-            SOLD_PRODUCT -> SoldProductFragment()
-            HIDDEN_PRODUCT -> HiddenProductFragment()
+            MY_SALES -> MySalesListFragment()
+            OTHER_SALES -> OtherSalesListFragment()
             else -> {
                 throw Error("Not valid fragment for designated page number.")
             }
@@ -23,7 +21,6 @@ class ProductUserSellPagerAdapter(activity: FragmentActivity) : FragmentStateAda
 }
 
 object ProductUserSellPageConst {
-    const val SELLING_PRODUCT = 0
-    const val SOLD_PRODUCT = 1
-    const val HIDDEN_PRODUCT = 2
+    const val MY_SALES = 0
+    const val OTHER_SALES = 1
 }
