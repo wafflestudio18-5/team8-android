b8b7bcf5d7a73bd351beebc83c6f244a5e2c49df
jusjinuk <58197958+jusjinuk@users.noreply.github.com>
2020-12-27 02:17:40 +0900

    Add basic NetworkModule.

--- PodoMarket/app/src/main/java/com/android/example/podomarket/di/NetworkModule.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/di/NetworkModule.kt
@@ -1,2 +1,35 @@
 package com.android.example.podomarket.di
 
+import com.android.example.podomarket.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+val networkModule = module {
+}
+
+private fun provideOkHttpClient() = if (BuildConfig.DEBUG) {
+    val loggingInterceptor = HttpLoggingInterceptor()
+    loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+} else OkHttpClient
+    .Builder()
+    .build()
+
+private fun provideRetrofit(
+    okHttpClient: OkHttpClient,
+    BASE_URL: String
+): Retrofit =
+    Retrofit.Builder()
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .client(okHttpClient)
+        .build()
+
