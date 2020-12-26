f09cdd481eda5f3e3016dd4e37166d95addfd062
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-26 12:23:36 +0900

    Create two PagerAdapters for UserSellActivity
    MySalesListPagerAdapter and OtherSalesListPagerAdapter

---
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/OtherSalesListPagerAdapter.kt
@@ -0,0 +1,29 @@
+package com.android.example.podomarket.ui.product
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.android.example.podomarket.ui.product.OtherSalesListPageConst.ALL_PRODUCT
+import com.android.example.podomarket.ui.product.OtherSalesListPageConst.SELLING_PRODUCT
+import com.android.example.podomarket.ui.product.OtherSalesListPageConst.SOLD_PRODUCT
+
+class OtherSalesListPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            ALL_PRODUCT -> AllProductFragment()
+            SELLING_PRODUCT -> SellingProductFragment()
+            SOLD_PRODUCT -> SoldProductFragment()
+            else -> {
+                throw Error("Not valid fragment for designated page number.")
+            }
+        }
+    }
+}
+
+object OtherSalesListPageConst {
+    const val ALL_PRODUCT = 0
+    const val SELLING_PRODUCT = 1
+    const val SOLD_PRODUCT = 2
+}
