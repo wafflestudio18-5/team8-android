db6af5dec52acbf10e545360d984ea994144ef11
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-26 12:34:55 +0900

    Connect Pager to Adapter

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/MySalesListFragment.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/MySalesListFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.android.example.podomarket.R
 import com.android.example.podomarket.databinding.FragmentMySalesListBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MySalesListFragment : Fragment() {
 
@@ -16,10 +18,20 @@ class MySalesListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val tabLayoutTextIdArray = arrayOf(R.string.selling, R.string.sold_out, R.string.hidden)
+
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_my_sales_list, container, false)
         binding.run {
-
+            pager.adapter = MySalesListPagerAdapter(requireActivity()) // 주의
+            pager.isUserInputEnabled = true
+            TabLayoutMediator(tabLayout, pager) { tab, position ->
+                tab.text = getString(tabLayoutTextIdArray[position])
+            }.attach()
+            toolBar.also { tb ->
+                tb.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+                tb.setNavigationOnClickListener { findNavController().navigateUp() }
+            }
         }
         return binding.root
     }
--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/OtherSalesListFragment.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/OtherSalesListFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.android.example.podomarket.R
 import com.android.example.podomarket.databinding.FragmentOtherSalesListBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class OtherSalesListFragment : Fragment() {
 
@@ -16,10 +18,20 @@ class OtherSalesListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val tabLayoutTextIdArray = arrayOf(R.string.all, R.string.selling, R.string.sold_out)
+
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_other_sales_list, container, false)
         binding.run {
-
+            pager.adapter = OtherSalesListPagerAdapter(requireActivity()) // 주
+            pager.isUserInputEnabled = true
+            TabLayoutMediator(tabLayout, pager) { tab, position ->
+                tab.text = getString(tabLayoutTextIdArray[position])
+            }.attach()
+            toolBar.also { tb ->
+                tb.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+                tb.setNavigationOnClickListener { findNavController().navigateUp() }
+            }
         }
         return binding.root
     }
