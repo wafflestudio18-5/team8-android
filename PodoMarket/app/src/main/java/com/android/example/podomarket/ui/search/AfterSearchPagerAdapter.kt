5ea48999525936e8d73aad4df4c35f9da60c3e40
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-26 15:52:32 +0900

    Create AfterSearchPagerAdapter and BeforeSearchPagerAdapter

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/AfterSearchPagerAdapter.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/AfterSearchPagerAdapter.kt
@@ -1,4 +1,26 @@
 package com.android.example.podomarket.ui.search
 
-class AfterSearchPagerAdapter {
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.android.example.podomarket.ui.search.AfterSearchPageConst.PRODUCT
+import com.android.example.podomarket.ui.search.AfterSearchPageConst.USER
+
+class AfterSearchPagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            PRODUCT -> AfterSearchProductFragment()
+            USER -> AfterSearchUserFragment()
+            else -> {
+                throw Error("Not valid fragment for designated page number.")
+            }
+        }
+    }
+}
+
+object AfterSearchPageConst {
+    const val PRODUCT = 0
+    const val USER = 1
 }
