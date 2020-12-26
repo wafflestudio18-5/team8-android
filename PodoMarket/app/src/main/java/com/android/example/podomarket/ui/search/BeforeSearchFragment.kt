ba27d34ebb3143d7cd2e9e51ff0ae25ae31e531a
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-26 15:58:41 +0900

    Connect Fragments to PagerAdapter

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/BeforeSearchFragment.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/BeforeSearchFragment.kt
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.android.example.podomarket.R
 import com.android.example.podomarket.databinding.FragmentBeforeSearchBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class BeforeSearchFragment : Fragment() {
 
@@ -16,11 +17,15 @@ class BeforeSearchFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding =
-            DataBindingUtil.inflate(inflater, R.layout.fragment_before_search, container, false)
+        val tabLayoutTextIdArray = arrayOf(R.string.used_trading, R.string.human)
 
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_before_search, container, false)
         binding.run {
-
+            pager.adapter = BeforeSearchPagerAdapter(requireActivity())
+            pager.isUserInputEnabled = true
+            TabLayoutMediator(tabLayout, pager) { tab, position ->
+                tab.text = getString(tabLayoutTextIdArray[position])
+            }.attach()
         }
         return binding.root
     }
