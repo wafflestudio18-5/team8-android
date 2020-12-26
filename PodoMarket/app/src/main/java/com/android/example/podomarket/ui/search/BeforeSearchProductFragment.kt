c45e1b55c7acfd8116346218c53a62e01a69640f
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-26 22:39:07 +0900

    Add DataBinding to BeforeSearchProductFragment

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/BeforeSearchProductFragment.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/BeforeSearchProductFragment.kt
@@ -1,6 +1,26 @@
 package com.android.example.podomarket.ui.search
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.FragmentBeforeSearchProductBinding
 
 class BeforeSearchProductFragment : Fragment() {
+
+    lateinit var binding: FragmentBeforeSearchProductBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_before_search_product, container, false)
+        binding.run {
+
+        }
+        return binding.root
+    }
+
 }
