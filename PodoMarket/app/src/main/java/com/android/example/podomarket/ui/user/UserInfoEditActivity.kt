da98f455d1916a440d2e11b8b4233d67a0968c63
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-31 02:13:43 +0900

    Add DataBinding

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/UserInfoEditActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/UserInfoEditActivity.kt
@@ -2,10 +2,18 @@ package com.android.example.podomarket.ui.user
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.ActivityUserInfoEditBinding
 
 class UserInfoEditActivity : AppCompatActivity() {
 
+    private val binding: ActivityUserInfoEditBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_user_info_edit
+        ) as ActivityUserInfoEditBinding
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info_edit)
