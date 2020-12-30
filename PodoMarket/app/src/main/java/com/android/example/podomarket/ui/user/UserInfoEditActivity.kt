fd7b5bd41b5ebd3a069dce5e57c17ea127632b02
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-31 02:25:07 +0900

    Add Navigation Icons

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/UserInfoEditActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/UserInfoEditActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.R
 import com.android.example.podomarket.databinding.ActivityUserInfoEditBinding
@@ -19,7 +20,28 @@ class UserInfoEditActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_user_info_edit)
+
+        binding.run {
+            toolBar.also { tb ->
+                tb.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+                tb.setNavigationOnClickListener { finish() }
+                tb.inflateMenu(R.menu.app_bar_activity_user_info_edit)
+                tb.setOnMenuItemClickListener {
+                    when (it.itemId) {
+                        R.id.complete_button -> {
+                            Toast.makeText(
+                                applicationContext,
+                                "프로필 수정 기능 미완성",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            finish()
+                        }
+                        else -> return@setOnMenuItemClickListener false
+                    }
+                    return@setOnMenuItemClickListener true
+                }
+            }
+        }
     }
 
     companion object {
