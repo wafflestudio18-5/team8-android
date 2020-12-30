6c8faeccffa4ef32d1d72a39fea97e14240e0b95
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-31 02:11:51 +0900

    Create UserInfoEditActivity

---
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/UserInfoEditActivity.kt
@@ -0,0 +1,14 @@
+package com.android.example.podomarket.ui.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.android.example.podomarket.R
+
+class UserInfoEditActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info_edit)
+    }
+
+}
