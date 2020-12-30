5cf5a44d920da1ad61556e0234f12cb68a024f2b
minchan <55829146+flux1126@users.noreply.github.com>
2020-12-31 03:50:44 +0900

    Update ProfileActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/ProfileActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/user/ProfileActivity.kt
@@ -3,6 +3,7 @@ package com.android.example.podomarket.ui.user
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -20,27 +21,62 @@ class ProfileActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding.run {
+
             toolBar.also { tb ->
                 tb.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
                 tb.setNavigationOnClickListener { finish() }
-                tb.inflateMenu(R.menu.app_bar_activity_profile)
-                tb.setOnMenuItemClickListener {
-                    when(it.itemId) {
-                        R.id.share_button -> Toast.makeText(
-                            this@ProfileActivity,
-                            "공유하기 기능 미완성",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        else -> return@setOnMenuItemClickListener false
+
+                val userId: Long = intent.getLongExtra("user_id", 0L)
+
+                if (userId == 0L) { // my profile
+
+                    tb.inflateMenu(R.menu.app_bar_activity_profile)
+                    tb.setOnMenuItemClickListener {
+                        when (it.itemId) {
+                            R.id.share_button -> Toast.makeText(
+                                this@ProfileActivity,
+                                "공유하기 기능 미완성",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            else -> return@setOnMenuItemClickListener false
+                        }
+                        return@setOnMenuItemClickListener true
                     }
-                    return@setOnMenuItemClickListener true
+
+                } else { // other profile
+
+                    myProfileEditBtn.visibility = View.GONE
+                    otherProfileFollowBtn.visibility = View.VISIBLE
+
+                    tb.inflateMenu(R.menu.app_bar_activity_profile_other)
+                    tb.setOnMenuItemClickListener {
+                        when (it.itemId) {
+                            R.id.share_button -> Toast.makeText(
+                                this@ProfileActivity,
+                                "공유하기 기능 미완성",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            R.id.more_button -> Toast.makeText(
+                                this@ProfileActivity,
+                                "추 기능 미완성",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            else -> return@setOnMenuItemClickListener false
+                        }
+                        return@setOnMenuItemClickListener true
+                    }
+
                 }
             }
+
         }
     }
 
     companion object {
-        fun intent(context: Context): Intent = Intent(context, ProfileActivity::class.java)
+        fun intentWithUserId(context: Context, user_id: Long): Intent // 자기 자신이면 user_id로 0을 전달
+            = Intent(context, ProfileActivity::class.java).apply { putExtra("user_id", user_id) }
+
     }
 }
