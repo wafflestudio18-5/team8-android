a723f2d4ea6f59a69a1b75be676b26962836326c
minchan <55829146+flux1126@users.noreply.github.com>
2021-01-03 21:36:41 +0900

    Add navigation logic for ProductDetailActivity

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductDetailActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/product/ProductDetailActivity.kt
@@ -2,6 +2,7 @@ package com.android.example.podomarket.ui.product
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,7 @@ import androidx.databinding.DataBindingUtil
 import com.android.example.podomarket.R
 import com.android.example.podomarket.databinding.ActivityProductDetailBinding
 import com.android.example.podomarket.ui.chat.ChatRoomActivity
+import com.android.example.podomarket.ui.user.ProfileActivity
 
 class ProductDetailActivity : AppCompatActivity() {
 
@@ -42,6 +44,37 @@ class ProductDetailActivity : AppCompatActivity() {
                     }
                     return@setOnMenuItemClickListener true
                 }
+            }
+            userInfoLayout.setOnClickListener {
+                //startActivity(ProfileActivity.intentWithUserId(productDetailViewModel.userId, this@ProductDetailActivity))
+            }
+            moreButton.setOnClickListener {
+                Toast.makeText(
+                    this@ProductDetailActivity,
+                    "더보 기능(미완성)",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            likeButton.setOnClickListener {
+                /*
+                productDetailViewModel.isLikeOn = !productDetailViewModel.isLikeOn
+
+                if (productDetailViewModel.isLikeOn) {
+                    likeButton.setColorFilter(R.color.purple_500)
+                } else {
+                    likeButton.setColorFilter(R.color.dark_gray)
+                }
+                */
+            }
+            callButton.setOnClickListener {
+                Toast.makeText(
+                    this@ProductDetailActivity,
+                    "전화하기 기능(미완성)",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            chatButton.setOnClickListener {
+                //startActivity(ChatRoomActivity.intentWithChatRoomId(productDetailViewModel.chatRoomId))
             }
         }
     }
