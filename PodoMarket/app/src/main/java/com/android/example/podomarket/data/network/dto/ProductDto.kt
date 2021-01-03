64c22e3e892704ca5c1a1a3cf7053db2f0bfa8cd
minchan <55829146+flux1126@users.noreply.github.com>
2021-01-04 01:37:23 +0900

    Update ProductDto.kt

--- PodoMarket/app/src/main/java/com/android/example/podomarket/data/network/dto/ProductDto.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/data/network/dto/ProductDto.kt
@@ -1,11 +1,12 @@
 package com.android.example.podomarket.data.network.dto
 
-data class ProductDto(
-    val product_id: Int,
+data class Product(
+    val id: Int,
     val name: String,
     val category: Int,
     val price: Int,
     val allow_suggest: Boolean,
+    val status: String,
     val distance_range: DistanceRange,
     val city_id: Int,
     val seller: Int,
@@ -19,4 +20,15 @@ data class DistanceRange(
     val x: Int,
     val y: Int,
     val z: Int
+)
+
+data class PaginationResponse(
+    val page: Page,
+    val products: List<Product>
+)
+
+data class Page(
+    val product_count: Int,
+    val page_count: Int,
+    val current_page: Int
 )
