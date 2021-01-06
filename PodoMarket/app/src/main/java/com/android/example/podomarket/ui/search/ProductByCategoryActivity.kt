dc4d4c2ae2169cf4c65097cdd5c4900f28048571
minchan <55829146+flux1126@users.noreply.github.com>
2021-01-07 01:58:11 +0900

    Create ProductByCategoryActivity

---
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/ProductByCategoryActivity.kt
@@ -0,0 +1,11 @@
+package com.android.example.podomarket.ui.search
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProductByCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_by_category)
+    }
+}
