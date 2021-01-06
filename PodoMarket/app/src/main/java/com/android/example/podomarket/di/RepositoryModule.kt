bc2c71759189059e0c2ccb866bcf9f749f654e03
minchan <55829146+flux1126@users.noreply.github.com>
2021-01-07 03:10:21 +0900

    Add Koin Repository Module

--- PodoMarket/app/src/main/java/com/android/example/podomarket/di/RepositoryModule.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/di/RepositoryModule.kt
@@ -1,2 +1,10 @@
 package com.android.example.podomarket.di
 
+import com.android.example.podomarket.data.repo.UserRepository
+import org.koin.dsl.module
+
+val repositoryModule = module {
+    single {
+        UserRepository(get(), get())
+    }
+}
