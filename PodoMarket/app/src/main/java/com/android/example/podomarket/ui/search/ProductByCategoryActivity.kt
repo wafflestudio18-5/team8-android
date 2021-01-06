6bb3c4429a1521e8b66520fb90254279ecda7fa8
minchan <55829146+flux1126@users.noreply.github.com>
2021-01-07 02:01:19 +0900

    Add DataBinding

--- PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/ProductByCategoryActivity.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/ProductByCategoryActivity.kt
@@ -2,10 +2,26 @@ package com.android.example.podomarket.ui.search
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.ActivityProductByCategoryBinding
 
 class ProductByCategoryActivity : AppCompatActivity() {
+
+    private val binding: ActivityProductByCategoryBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_product_by_category
+        ) as ActivityProductByCategoryBinding
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_by_category)
+        binding.run {
+            toolBar.also { tb ->
+                tb.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+                tb.setNavigationOnClickListener { finish() }
+            }
+        }
     }
 }
