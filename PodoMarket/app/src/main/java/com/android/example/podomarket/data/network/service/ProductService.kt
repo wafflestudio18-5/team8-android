30f4e054ce5ca2dbbb849493dd916bacd8fdbfcd
minchan <55829146+flux1126@users.noreply.github.com>
2021-01-07 02:19:33 +0900

    Add getProduct Function

--- PodoMarket/app/src/main/java/com/android/example/podomarket/data/network/service/ProductService.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/data/network/service/ProductService.kt
@@ -4,10 +4,7 @@ import com.android.example.podomarket.data.network.dto.DistanceRange
 import com.android.example.podomarket.data.network.dto.Product
 import io.reactivex.rxjava3.core.Single
 import retrofit2.Response
-import retrofit2.http.Field
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.PUT
+import retrofit2.http.*
 
 interface ProductService {
 
@@ -20,9 +17,14 @@ interface ProductService {
         @Field("distance/range") distance_range: DistanceRange,
         @Field("city_id") city_id: Int,
         @Field("seller") seller: Int
-    )
+    ): Single<Response<Product>>
 
     @GET("api/v1/product")
     fun getProductList(): Single<Response<List<Product>>>
+
+    @GET("api/v1/product/{product_id}")
+    fun getProduct(
+        @Path("product_id") product_id: Int
+    )
 
 }
