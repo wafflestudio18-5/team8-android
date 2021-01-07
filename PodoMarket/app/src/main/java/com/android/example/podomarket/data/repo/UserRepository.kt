770ff3711b9fbfb72d733d22d9803228a5e1ff1f
김민찬 <55829146+flux1126@users.noreply.github.com>
2021-01-08 00:20:12 +0900

    Create getUserInfo() function and getMyInfo() function in UserRepository (#50)
    * Create UserInfo
    
    * Update UserInfo.kt
    
    * Update UserInfo.kt
    
    * Replace UserInfo.kt to getMyInfo() function in UserRepository
    
    * Update UserRepository.kt
    
    * Add Koin Repository Module
    
    * Fix minor nitpicks.
    
    Co-authored-by: jusjinuk <jusjinuk@gmail.com>

--- PodoMarket/app/src/main/java/com/android/example/podomarket/data/repo/UserRepository.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/data/repo/UserRepository.kt
@@ -1,4 +1,35 @@
 package com.android.example.podomarket.data.repo
 
-class UserRepository {
+import android.content.SharedPreferences
+import com.android.example.podomarket.data.network.dto.UserDto
+import com.android.example.podomarket.data.network.service.UserService
+import com.google.gson.Gson
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class UserRepository(private val sp: SharedPreferences, private val service: UserService) {
+
+    fun getUserInfo(user_id: Int) = service.getUserById(user_id)
+
+    fun getMyInfo(): UserDto? {
+        var userDto: UserDto? = null
+        val json = sp.getString(USER_INFO, null)
+
+        if (json != null) { // load from memory
+            userDto = Gson().fromJson(json, UserDto::class.java)
+        } else { // load from network
+            val singleResponse = service.getUserMe()
+            singleResponse
+                .subscribeOn(Schedulers.io())
+                .subscribe { response ->
+                    userDto = response.body()
+                    val newJson = Gson().toJson(userDto)
+                    sp.edit().putString(USER_INFO, newJson).apply() // save to memory
+                }
+        }
+        return userDto
+    }
+
+    companion object {
+        const val USER_INFO = "user_info"
+    }
 }
