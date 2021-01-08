384ec34cb7c57d14fe32d930873223f0ab206698
김민찬 <55829146+flux1126@users.noreply.github.com>
2021-01-08 23:10:16 +0900

    ProductByCategoryActivity (#56)
    * Create ProductByCategoryActivity
    
    * Add DataBinding
    
    * Add Incoming Intent Funciton
    
    * Create ViewModel and Adapter
    
    * Add RecyclerView

---
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/ui/search/category/ProductByCategoryActivity.kt
@@ -0,0 +1,41 @@
+package com.android.example.podomarket.ui.search.category
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.android.example.podomarket.R
+import com.android.example.podomarket.databinding.ActivityProductByCategoryBinding
+
+class ProductByCategoryActivity : AppCompatActivity() {
+
+    private val binding: ActivityProductByCategoryBinding by lazy {
+        DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_product_by_category
+        ) as ActivityProductByCategoryBinding
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.run {
+            toolBar.also { tb ->
+                tb.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24)
+                tb.setNavigationOnClickListener { finish() }
+            }
+        }
+    }
+
+    companion object {
+        private const val CATEGORY_ID = "category_id"
+
+        fun intentWithCategoryId(category_id: Long, context: Context): Intent =
+            Intent(context, ProductByCategoryActivity::class.java).apply {
+                putExtra(
+                    CATEGORY_ID,
+                    category_id
+                )
+            }
+    }
+}
