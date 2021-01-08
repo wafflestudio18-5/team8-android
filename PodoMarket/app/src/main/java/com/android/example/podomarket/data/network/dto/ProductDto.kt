d3d70caec46a437dc99bd1e24bae4156a5fdee6b
김민찬 <55829146+flux1126@users.noreply.github.com>
2021-01-09 00:02:24 +0900

    Network Code (#54)
    * Create ProductDto.kt
    
    * Update ProductDto.kt
    
    * Create ProductService.kt
    
    * Add getProduct Function
    
    * Update ProductDto
    
    * Update UserDto.kt
    
    * Update UserService.kt
    
    * Add logout funciton to UserService
    
    * Update UserDto.kt

---
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/data/network/dto/ProductDto.kt
@@ -0,0 +1,34 @@
+package com.android.example.podomarket.data.network.dto
+
+data class ProductDto(
+    val id: Int,
+    val name: String,
+    val category: Int,
+    val price: Int,
+    val allow_suggest: Boolean,
+    val status: String,
+    val distance_range: DistanceRange,
+    val city_id: Int,
+    val seller: Int,
+    val buyer: Int,
+    val count_comments: Int,
+    val count_likes: Int,
+    val count_views: Int
+)
+
+data class DistanceRange(
+    val x: Int,
+    val y: Int,
+    val z: Int
+)
+
+data class PaginationResponse(
+    val page: Page,
+    val products: List<ProductDto>
+)
+
+data class Page(
+    val product_count: Int,
+    val page_count: Int,
+    val current_page: Int
+)
