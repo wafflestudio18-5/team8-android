ca5ad86e788ee590c351544c1619cd5705b62878
김진욱 <58197958+jusjinuk@users.noreply.github.com>
2021-01-09 00:21:07 +0900

    Revert "Network Code (#54)" (#59)
    This reverts commit d3d70caec46a437dc99bd1e24bae4156a5fdee6b.

--- PodoMarket/app/src/main/java/com/android/example/podomarket/data/network/dto/UserDto.kt
+++ PodoMarket/app/src/main/java/com/android/example/podomarket/data/network/dto/UserDto.kt
@@ -1,19 +1,18 @@
 package com.android.example.podomarket.data.network.dto
 
+// TODO
+// Should add badges, Products_bought, Products_sold
 data class UserDto(
-    val id: Int,
+    val user_id: Int,
     val full_name: String,
     val nickname: String,
     val image: String,
-    val temperature: Float,
-    val badges: String,
-    val products_bought: Int,
-    val products_sold: Int,
+    val temperature: Double,
     val token: String
 )
 
 data class CityDto(
-    val id: Int,
+    val city_id: Int,
     val name: String,
     val location: String
 )
